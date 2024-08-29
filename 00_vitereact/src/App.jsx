dd5fc681d186618c3390b5553beea5e25c7f00fe
wespy <wespy07@gmail.com>
2024-08-29 11:57:28 +0530

    make a test component

--- 00_vitereact/src/App.jsx
+++ 00_vitereact/src/App.jsx
@@ -1,8 +1,13 @@
+import TestFn from './test.jsx'
 
 function App() {
 
   return (
-    <h1>Hello Friend | Vite</h1>
+    <>
+      <h1>Hello Friend | Vite</h1>
+      < TestFn />
+      <p>test para</p>
+    </>
   )
 }
 export default App
