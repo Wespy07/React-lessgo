faffa65d7613e931dd0a2d0eb8cfcf0f05173556
wespy <wespy07@gmail.com>
2024-09-03 12:32:42 +0530

    understanding react router with a multipage website project

---
+++ 06_reactrouter/src/main.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/Home/Home.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import About from './components/About/About.jsx'
+// import Root from './Root.jsx' 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path:'',
+        element: <Home/>
+      },
+      {
+        path:'contact',
+        element: <Contact/>
+      },
+      {
+        path:'about',
+        element: <About/>
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    < RouterProvider router={router}/>
+  </StrictMode>,
+)
