a63fe081a0034334e5f76c764ed0d6b83c5232b5
wespy <wespy07@gmail.com>
2024-09-13 12:57:01 +0530

    basics of usecontext

--- 07_usecontext/src/App.jsx
+++ 07_usecontext/src/App.jsx
@@ -1,10 +1,15 @@
+import Login from "./components/login"
+import Profile from "./components/Profile"
+import UserContextProvider from "./contexts/userContextProvider"
 
 function App() {
 
   return (
-    <>
+    <UserContextProvider>
       <h1>Use context</h1>
-    </>
+      < Login />      
+      < Profile />      
+    </UserContextProvider>
   )
 }
 
