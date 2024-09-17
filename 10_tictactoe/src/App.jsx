d0790d1d465d37c66c6f52b8df9cc861a835ba5c
wespy <wespy07@gmail.com>
2024-09-17 12:21:27 +0530

    fix the UI

--- 10_tictactoe/src/App.jsx
+++ 10_tictactoe/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Board from './TictactoeGame/Board'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Tic Tac Toe</h1>
+    <>
+      <h1>Tic Tac Toe</h1>
+      < Board />
+    </>
   )
 }
 
---
+++ 10_tictactoe/src/TictactoeGame/Square.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Square() {
+  return (
+    <div className='board-square'>
+        <h4>X</h4>
+    </div>
+  )
+}
+
+export default Square
