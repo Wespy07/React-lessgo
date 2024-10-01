be57ee8ea640afb7f11dacbccc04ac923e300b80
wespy <wespy07@gmail.com>
2024-10-01 13:00:48 +0530

    fix cart items and show total

--- 13_cartContext/src/components/Cart.jsx
+++ 13_cartContext/src/components/Cart.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CartContext } from '../context/CartContext'
+import formatter from './NumberFormatter'
 
 function Cart() {
+  const cart = useContext(CartContext)
+  const total = cart.items.reduce((a,b) => a + Number(b.price), 0)
+
   return (
     <div className='mt-3 bg-gray-900 py-4 rounded-lg text-xl font-semibold'>
-        <li>laptop - 80000</li>
-
-        <h4>Total: Rs </h4>
+      <h3 className='my-3'>Cart</h3>
+      {
+        cart && cart.items.map((item, index) => (
+          <li key={index}>{item.name} - Rs {formatter.format(item.price)}</li>
+          // console.log(item)
+        ))
+      }
+      <h4 className='text-2xl'>Total: Rs <span className='font-extrabold'>{formatter.format(total)}</span></h4>
     </div>
   )
 }
--- 13_cartContext/src/components/Item.jsx
+++ 13_cartContext/src/components/Item.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CartContext } from '../context/CartContext'
+import formatter from './NumberFormatter'
 
 function Item(props) {
+  const cart = useContext(CartContext)
+  // console.log(cart)
   return (
     <div className='border rounded-lg py-3 mt-3'>
-        <h4>{props.title}</h4>
-        <p>Price: Rs {props.price}</p>
-        <button className='bg-gray-500'>Add to Cart</button>
+      <h4>{props.title}</h4>
+      <p>Price: Rs {props.price}</p>
+      <button className='bg-gray-500' onClick={() =>
+        cart.setItems(
+          [...cart.items, { name: props.title, price: props.price }]
+        )}>Add to Cart
+      </button>
     </div>
   )
 }
