b12e614976413504df60386ab459365c7a3218ad
wespy <wespy07@gmail.com>
2024-10-08 14:27:22 +0530

    add routing and app logics

--- 15_RoutingDOM/src/components/PostsPage.jsx
+++ 15_RoutingDOM/src/components/PostsPage.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 function PostsPage() {
-  return (
-    <div>PostsPage</div>
+    const [post, setPost] = useState(null)
+
+    const params = useParams()
+    console.log(params)
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
+            .then((res) => res.json())
+            .then((data) => setPost(data))
+    }, [])
+
+    console.log(post)
+    return (
+        <div className='post-container'>
+            {
+                post ? 
+                <>
+                    <h1>{post.title}</h1>
+                    <h6>Post id: {post.id}</h6>
+                    <p>{post.body}</p>
+                </> : <p>Please wait...</p>
+            }
+        </div>
   )
 }
 
