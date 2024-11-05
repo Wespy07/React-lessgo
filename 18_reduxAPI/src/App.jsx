d1855d0497fbe259bd25eefb1252c22b8918ff71
wespy <wespy07@gmail.com>
2024-11-05 12:56:41 +0530

    initial commit and include redux-toolkit

---
+++ 18_reduxAPI/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h2>API Calling in Redux using redux-toolkit</h2>
+  )
+}
+
+export default App
