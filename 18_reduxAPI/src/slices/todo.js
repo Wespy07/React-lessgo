557302d5ba2eedd834014b0a9a6aa8cde44b7cf0
wespy <wespy07@gmail.com>
2024-11-06 16:00:15 +0530

    understanding thunk in redux

--- 18_reduxAPI/src/slices/todo.js
+++ 18_reduxAPI/src/slices/todo.js
@@ -1,7 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchTodos = createAsyncThunk('fetchTodos', async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+    return response.json()
+})
 
 const todoSlice = createSlice({
-    name: 'todo'
+    name: 'todo',
+    initialState: {
+        isLoading: false,
+        data: null
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchTodos.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.data = action.payload;
+        })
+    }
 })
 
 export default todoSlice.reducer
